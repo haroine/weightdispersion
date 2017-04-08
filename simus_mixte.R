c2b21c58a41d55c5f3147cf73e0b5ac90f848e4b
Antoine R <antoine.rebecq@m4x.org>
2017-04-08 09:50:52 -0400

    Add simulations under the model

--- simus_mixte.R
+++ simus_mixte.R
@@ -1,77 +1,101 @@
 
-graph_mixte <- function(alpha) {
-
-## Simulations mixte
-df <- df_copy
-df$piks <- alpha * n * df$X / sum(df$X) + (1-alpha) * n/N
-
-nSimus <- 1000
-simus_pps <- foreach(kSimus=1:nSimus, .combine=rbind) %do% {
+## Under the model probability
+graph_mixte_model <- function(alpha, df, nSimus=1000) {
   
-  sampledId <- which(UPpivotal(df$piks) == 1)
-  
-  df[sampledId,"sampled"] <- df[sampledId,"sampled"]+1
-  
-  df_sample <- df[sampledId,]
-  modele_estime <- lm(Y ~ X, data = df_sample, weights = 1/df_sample$piks)
-  beta_estime <- unname(modele_estime$coefficients[2])
-  alpha_estime <- unname(modele_estime$coefficients[1])
-  
-  est_mean_X <- 1/N*sum(df_sample$X / df_sample$piks)
-  est_q95_X <- unname(wtd.quantile(df_sample$X, 
-                                   weights = 1/df_sample$piks, probs = c(0.95)))
-  
-  c(alpha_estime, beta_estime, est_mean_X, est_q95_X)
+df$piks <- 0
+## Compute the piks
+for(k in 1:N_simus_models) {
+  df[(1:N)+(k-1)*N,"piks"] <- alpha * n * df[(1:N)+(k-1)*N,"X"] / sum(df[(1:N)+(k-1)*N,"X"]) + (1-alpha) * n/N
 }
 
-df$sampled <- df$sampled / nSimus
-simus_pps <- data.frame(simus_pps, stringsAsFactors = F)
-colnames(simus_pps) <- c("alpha_estime", "beta_estime", 
+simus_pps_model <- foreach(kSimus_model=1:N_simus_models, .combine=rbind) %do% {
+  df_k <- df[df$k_sim == kSimus_model,]
+    
+  ## Simulations mixte
+  df_k$sampled <- 0
+  # df_k$piks <- alpha * n * df_k$X / sum(df_k$X) + (1-alpha) * n/N
+  
+  simus_pps <- foreach(kSimus=1:nSimus, .combine=rbind) %do% {
+    
+    sampledId <- which(UPpivotal(df_k$piks) == 1)
+    
+    df_k[sampledId,"sampled"] <- df_k[sampledId,"sampled"]+1
+    
+    df_k_sample <- df_k[sampledId,]
+    modele_estime <- lm(Y ~ X, data = df_k_sample, weights = 1/df_k_sample$piks)
+    beta_estime <- unname(modele_estime$coefficients[2])
+    alpha_estime <- unname(modele_estime$coefficients[1])
+    
+    est_mean_X <- 1/N*sum(df_k_sample$X / df_k_sample$piks)
+    est_q95_X <- unname(wtd.quantile(df_k_sample$X, 
+                                     weights = 1/df_k_sample$piks, probs = c(0.95)))
+    
+    c(alpha_estime, beta_estime, est_mean_X, est_q95_X)
+  }
+simus_pps
+}
+
+simus_pps_model <- data.frame(simus_pps_model, stringsAsFactors = F)
+row.names(simus_pps_model) <- NULL
+colnames(simus_pps_model) <- c("alpha_estime", "beta_estime", 
                          "est_mean_X", "est_q95_X")
 
+
 ## Parameters of the confidence region
-beta_1 <- unname(quantile(simus_pps$beta_estime, probs=c(0.95)))
-beta_2 <- unname(quantile(simus_pps$beta_estime, probs=c(0.05)))
-alpha_1 <- unname(quantile(simus_pps$alpha_estime, probs=c(0.95)))
-alpha_2 <- unname(quantile(simus_pps$alpha_estime, probs=c(0.05)))
+beta_1 <- unname(quantile(simus_pps_model$beta_estime, probs=c(0.95)))
+beta_2 <- unname(quantile(simus_pps_model$beta_estime, probs=c(0.05)))
+alpha_1 <- unname(quantile(simus_pps_model$alpha_estime, probs=c(0.95)))
+alpha_2 <- unname(quantile(simus_pps_model$alpha_estime, probs=c(0.05)))
 
 ## Estimators of X : mean and quantile
-X_mean_1 <- unname(quantile(simus_pps$est_mean_X, probs=c(0.95)))
-X_mean_2 <- unname(quantile(simus_pps$est_mean_X, probs=c(0.05)))
-X_q95_1 <- unname(quantile(simus_pps$est_q95_X, probs=c(0.95)))
-X_q95_2 <- unname(quantile(simus_pps$est_q95_X, probs=c(0.05)))
+X_mean_1 <- unname(quantile(simus_pps_model$est_mean_X, probs=c(0.95)))
+X_mean_2 <- unname(quantile(simus_pps_model$est_mean_X, probs=c(0.05)))
+X_q95_1 <- unname(quantile(simus_pps_model$est_q95_X, probs=c(0.95)))
+X_q95_2 <- unname(quantile(simus_pps_model$est_q95_X, probs=c(0.05)))
 
-sd_X_mean <- sd(simus_pps$est_mean_X)
-sd_X_q95 <- sd(simus_pps$est_q95_X)
-sd_beta <- sd(simus_pps$beta_estime)
-sd_alpha <- sd(simus_pps$alpha_estime)
+sd_X_mean <- sd(simus_pps_model$est_mean_X)
+sd_X_q95 <- sd(simus_pps_model$est_q95_X)
+sd_beta <- sd(simus_pps_model$beta_estime)
+sd_alpha <- sd(simus_pps_model$alpha_estime)
 
-vrai_mean_X <- mean(df$X)
-vrai_q95_X <- unname(quantile(df$X, probs=c(0.95)))
+real_X_mean <- real_values[1]
+real_X_q95 <- real_values[2]
 
-plot_pps_simus <- ggplot(df, aes(x=X, y=Y, colour=sampled)) +
+## Mean squared errors
+mse_X_mean <- sd_X_mean**2 + (mean(simus_pps_model$est_mean_X) - real_X_mean)**2
+mse_X_q95 <- sd_X_q95**2 + (mean(simus_pps_model$est_q95_X) - real_X_q95)**2
+mse_beta <- sd_beta**2 + (mean(simus_pps_model$beta_estime) - real_beta)**2
+mse_alpha <- sd_alpha**2 + (mean(simus_pps_model$alpha_estime) - real_alpha)**2
+
+df$sampled <- df$piks
+# print(sum(df$piks))
+df_reduced <- df[UPpivotal(df$piks) == 1,]
+
+plot_pps_sims_model <- ggplot(df_reduced, aes(x=X, y=Y, colour=sampled)) +
   geom_point(shape=21) +
-  geom_abline(slope = vrai_beta, intercept = vrai_alpha) +
+  geom_abline(slope = real_beta, intercept = real_alpha) +
   geom_abline(slope = beta_1, intercept = alpha_1, 
               colour="red", linetype="dashed") +
   geom_abline(slope = beta_2, intercept = alpha_2, 
               colour="red", linetype="dashed") +
-  # geom_vline(xintercept=c(X_mean_1,X_mean_2), 
+  # geom_vline(xintercept=c(X_mean_1,X_mean_2),
   #            colour="blue", linetype="dashed") +
   # geom_vline(xintercept=X_mean_2, colour="blue", linetype="dashed") +
   # geom_vline(xintercept=X_q95_1, colour="blue", linetype="dashed") +
   # geom_vline(xintercept=c(X_q95_1,X_q95_2),
   #            colour="blue", linetype="dashed") +
-  # geom_vline(xintercept=vrai_mean_X, colour="blue") +
-  # geom_vline(xintercept=vrai_q95_X, colour="blue") +
+  # geom_vline(xintercept=real_X_mean, colour="blue") +
+  # geom_vline(xintercept=real_X_q95, colour="blue") +
   scale_color_continuous(low="#ffffff", high = "#ff0000")
   # ylim(c(0.85*vrai_alpha,vrai_alpha/0.85))
   # scale_x_continuous(trans=custom_trans, 
   #                    breaks = c(500,1000,2000,5000,10000,20000))
 
-  returnList <- list(plot_pps_simus, c(X_mean_1,X_mean_2), 
-                     c(X_q95_1,X_q95_2), sd_X_mean, sd_X_q95, c(sd_alpha, sd_beta))
-  names(returnList) <- c("plot","ci_mean","ci_q95","sd_mean","sd_95","sd_param")
+  returnList <- list(plot_pps_sims_model, c(X_mean_1,X_mean_2), 
+                     c(X_q95_1,X_q95_2), sd_X_mean, sd_X_q95, c(sd_alpha, sd_beta),
+                     mse_X_mean, mse_X_q95, c(mse_alpha, mse_beta))
+  names(returnList) <- c("plot","ci_mean","ci_q95","sd_mean","sd_95","sd_param",
+                         "mse_mean","mse_95","mse_param")
   return(returnList)
 
 }
--- simus_modele_lineaire.R
+++ simus_modele_lineaire.R
@@ -8,26 +8,54 @@ library(scales)
 
 
 set.seed(1005192119)
-N <- 1000
-n <- 50
+N <<- 1000
+n <<- 50
+N_simus_models <<- 31
 
-## Auxiliary variable (revenue)
-X <- rpareto(N,2000,100)
+real_beta <- -0.1
+real_alpha <- 10000
+nSim_design <- 31
 
-vrai_beta <- -0.1
-vrai_alpha <- 10000
-## Variable of interest: linear model, 
-## (weak) negative correlation with the revenue
-Y <- vrai_beta*X + rnorm(N,0,1500) + vrai_alpha
+location_pareto <- 2000
+dispersion_pareto <- 100
+sigma_norm <- 1500
 
-df <- data.frame(X,Y)
+## compute real values of X under the model
+# real_values <- foreach(k_values=1:100000, .combine=rbind) %do% {
+#   X <- rpareto(N,location_pareto,dispersion_pareto)
+#   
+#   c(mean(X), quantile(X, probs = c(0.95)))
+# }
+# 
+# real_values <- colMeans(real_values)
+real_values <<- c(1999.584,6003.200)
+
+## Simulate models
+df <- data.frame(matrix(0,nrow = N*N_simus_models, ncol = 3))
+names(df) <- c("X","Y","k_sim")
+
+for(k in 1:N_simus_models) {
+  
+  df[(1:N)+(k-1)*N,"k_sim"] <- k
+  
+  ## Auxiliary variable (revenue)
+  X <- rpareto(N,location_pareto,dispersion_pareto)
+  df[(1:N)+(k-1)*N,"X"] <- X
+  
+  ## Variable of interest: linear model, 
+  ## (weak) negative correlation with the revenue
+  df[(1:N)+(k-1)*N,"Y"] <- real_beta*X + rnorm(N,0,sigma_norm) + real_alpha
+  
+}
+
+
 
 ## Plot of the model
-plot_lm <- ggplot(df, aes(x=X, y=Y)) +
-  geom_point() +
-  geom_abline(slope = vrai_beta, intercept = vrai_alpha)
-
-print(plot_lm)
+# plot_lm <- ggplot(df, aes(x=X, y=Y)) +
+#   geom_point() +
+#   geom_abline(slope = vrai_beta, intercept = vrai_alpha)
+# 
+# print(plot_lm)
 
 ## Custom scaling
 custom_trans <- trans_new(name="custom_trans", transform = function(x) {
@@ -38,64 +66,65 @@ inverse = function(x) {
 })
 
 ## Plot the model, scaled
-plot_lm_scaled <- ggplot(df, aes(x=X, y=Y)) +
-  geom_point() +
-  geom_abline(slope = vrai_beta, intercept = vrai_alpha) +
-  scale_x_continuous(trans=custom_trans, 
-                     breaks = c(500,1000,2000,5000,10000,20000))
-
-print(plot_lm_scaled)
-
-########## Sampling
-df$sampled <- 0
-df_copy <- df
+# plot_lm_scaled <- ggplot(df, aes(x=X, y=Y)) +
+#   geom_point() +
+#   geom_abline(slope = vrai_beta, intercept = vrai_alpha) +
+#   scale_x_continuous(trans=custom_trans, 
+#                      breaks = c(500,1000,2000,5000,10000,20000))
+# 
+# print(plot_lm_scaled)
 
 #### Generation of standard error plot
 
 listPlotsArticle <- list()
 points <- seq(0,1,by=0.05)
+# points <- seq(0,1,by=0.5)
+
 
 for(k in 1:length(points)) {
-  print(k)
-  listPlotsArticle[[k]] <- graph_mixte(points[k])
+  print(paste(k,"-",points[k]))
+  listPlotsArticle[[k]] <- graph_mixte_model(points[k], df, nSimus=nSim_design)
 }
 
 len_list <- length(listPlotsArticle)
-sdmean <- rep(0, len_list)
-sd95 <- rep(0, len_list)
-sdBeta <- rep(0, len_list)
-sdAlpha <- rep(0, len_list)
+msemean <- rep(0, len_list)
+mse95 <- rep(0, len_list)
+mseBeta <- rep(0, len_list)
+mseAlpha <- rep(0, len_list)
 
 for(j in 1:len_list) {
-  sdmean[j] <- listPlotsArticle[[j]]$sd_mean
-  sd95[j] <- listPlotsArticle[[j]]$sd_95
-  sdBeta[j] <- listPlotsArticle[[j]]$sd_param[2]
-  sdAlpha[j] <- listPlotsArticle[[j]]$sd_param[1]
+  msemean[j] <- listPlotsArticle[[j]]$mse_mean
+  mse95[j] <- listPlotsArticle[[j]]$mse_95
+  mseBeta[j] <- listPlotsArticle[[j]]$mse_param[2]
+  mseAlpha[j] <- listPlotsArticle[[j]]$mse_param[1]
 }
 
-df_sd <- data.frame(points,sdmean,sd95,sdAlpha,sdBeta)
+df_mse <- data.frame(points,msemean,mse95,mseAlpha,mseBeta)
 
-df_sd$sdmean_norm <- df_sd$sdmean / max(df_sd$sdmean)
-df_sd$sd95_norm <- df_sd$sd95 / max(df_sd$sd95)
-df_sd$sdAlpha_norm <- df_sd$sdAlpha / max(df_sd$sdAlpha)
-df_sd$sdBeta_norm <- df_sd$sdBeta / max(df_sd$sdBeta)
+df_mse$msemean_norm <- df_mse$msemean / max(df_mse$msemean)
+df_mse$mse95_norm <- df_mse$mse95 / max(df_mse$mse95)
+df_mse$mseAlpha_norm <- df_mse$mseAlpha / max(df_mse$mseAlpha)
+df_mse$mseBeta_norm <- df_mse$mseBeta / max(df_mse$mseBeta)
 
-names(df_sd)[1] <- "alpha"
+names(df_mse)[1] <- "alpha"
 
 currentAlpha <- 0.2
 point_df <- data.frame(rep(currentAlpha,3),
-     c(df_sd[df_sd$alpha==currentAlpha,c("sdmean_norm","sd95_norm","sdBeta_norm")]))
+     c(df_mse[df_mse$alpha==currentAlpha,c("msemean_norm","mse95_norm","mseBeta_norm")]))
 names(point_df)[1] <- "alpha"
 
-plot_sd <- ggplot(df_sd, aes(x=alpha)) +
-  geom_smooth(aes(y=sdmean_norm, colour="mean"),se=0) +
-  geom_smooth(aes(y=sd95_norm, colour="q95"),se=0) +
-  geom_smooth(aes(y=sdBeta_norm, colour="beta"),se=0) +
+plot_mse <- ggplot(df_mse, aes(x=alpha)) +
+  geom_smooth(aes(y=msemean_norm, colour="mean"),se=0) +
+  geom_smooth(aes(y=mse95_norm, colour="q95"),se=0) +
+  geom_smooth(aes(y=mseBeta_norm, colour="beta"),se=0) +
   ylab("Standard error (as percent of max)") +
   scale_color_manual(values=c("beta"="forestgreen","mean"="red","q95"="blue")) +
   guides(colour=guide_legend(title=NULL))
 
-print(plot_sd)
+print(plot_mse)
+
+print(object.size(listPlotsArticle), units="Mb")
 
 # save(listPlotsArticle, custom_trans,
-#      df_sd, plot_sd, vrai_alpha, vrai_beta, file="listPlots.RData")
+#      df_mse, plot_mse, real_alpha, real_alpha, real_values,
+#      file="listPlots_model.RData")
