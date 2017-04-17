bf101ad8be17137139fae8e42ae2fd1441435b30
Antoine R <antoine.rebecq@m4x.org>
2017-04-17 15:34:47 -0400

    Generate graph

--- dispersion_weights_graph.R
+++ dispersion_weights_graph.R
@@ -7,20 +7,22 @@ Y <- real_beta*X + rnorm(N,0,sigma_norm) + real_alpha
 
 df_dispersion <- data.frame(X,Y)
 
-muPoints <- seq(0,1,by = 0.01)
+muPoints <- seq(0,1,by = 0.005)
 disp_weights <- rep(0,length(muPoints))
 
 for(k in 1:length(muPoints)) {
   currentMu <- muPoints[k]
   piks <- currentMu * n * X / sum(X) + (1-currentMu) * n/N
-  disp_weights[k] <- var(piks)
+  disp_weights[k] <- var(1/piks)
 }
 
 df_disp_weights <- data.frame(muPoints, disp_weights)
 
 plot_disp_weights <- ggplot(df_disp_weights) +
-  geom_smooth(aes(x=muPoints,y=disp_weights)) +
+  # geom_smooth(aes(x=muPoints,y=disp_weights), se=0) +
+  geom_point(aes(x=muPoints,y=disp_weights)) +
   ylab("Dipsersion of the weights") +
-  xlab("mu")
+  xlab("mu") +
+  scale_y_log10()
 
 print(plot_disp_weights)
